5c574411a7b4962320dbd57720515050e07ecd19
wo658 <whdgns2347@gmail.com>
2024-08-05 16:09:43 +0900

    txtFile

--- ConsoleApplication1/main.cpp
+++ ConsoleApplication1/main.cpp
@@ -9,7 +9,14 @@ using namespace std;
 
 // RNN 으로 특정 단어가 어떤 언어인지 예측하기.
 
+// 1 . RNN Dataset 읽어 와서 각 언어별로 분류하기
 
+// 1 - 1 . 
+// 2 . RNN 구조 구축
+// 2 - 1 . 
+
+// 3 . RNN 학습 진행
+// 4 . 모델 평가
 
 
 int main()
